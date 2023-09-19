9727db16c45cb7278d2e613200da4a0c8e75e996
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-09-19 18:40:54 +0530

    chore: add courses header, footer

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,3 +1,4 @@
+import MainLayout from "src/layouts/main";
 import ElearningCoursesView from "src/sections/_elearning/view/elearning-courses-view";
 import Header from "../../layouts/main/";
 
@@ -8,5 +9,9 @@ export const metadata = {
 };
 
 export default function ElearningCoursesPage() {
-  return <ElearningCoursesView />;
+  return (
+    <MainLayout>
+      <ElearningCoursesView />
+    </MainLayout>
+  );
 }
